44ac3e00afc6537ae2c529b1b17ea209f9810926
Thierry Jegen <s8thjege@stud.uni-saarland.de>
2024-02-22 09:57:50 +0100

    Smaller Cleanup

--- extension-logic/src/model/constants.ts
+++ extension-logic/src/model/constants.ts
@@ -8,51 +8,50 @@ export const URL_GAME = "http://localhost:5173";
 export const WEBSOCKETS_BASE = "https://localhost:7262/userHub";
 
 export const FILE_PATHS = {
-	FOREGROUND_JS: "/js/foreground.js",
-	FOREGROUND_CSS: "/css/index.css",
-	FOREGROUND_CSS_Game: "/css/indexGame.css",
+  FOREGROUND_JS: "/js/foreground.js",
+  FOREGROUND_CSS: "/css/index.css",
 };
 
 export const Ids = {
-	ContextMenuId: "context-menu",
-	IconBaseId: "icon-",
-	TextBaseId: "text-",
-	SpanBaseId: "span-",
-	ActiveTaskInformation: "active-task-information",
-	TeamDominationBorder: "team-domination-border",
-	TeamDominationContainer: "team-domination-container",
-	NotificationBaseId: "injected-notification-",
-	UserCountElement: "userCountElement",
-	DeleteButton: "delete-button",
-	RatingButton: "rating-button",
-	WellFormulatedButton: "well-formulated-button",
-	BadlyFormulatedButton: "badly-formulated-button",
-	SemanticMarkButton: "semantic-button",
+  ContextMenuId: "context-menu",
+  IconBaseId: "icon-",
+  TextBaseId: "text-",
+  SpanBaseId: "span-",
+  ActiveTaskInformation: "active-task-information",
+  TeamDominationBorder: "team-domination-border",
+  TeamDominationContainer: "team-domination-container",
+  NotificationBaseId: "injected-notification-",
+  UserCountElement: "userCountElement",
+  DeleteButton: "delete-button",
+  RatingButton: "rating-button",
+  WellFormulatedButton: "well-formulated-button",
+  BadlyFormulatedButton: "badly-formulated-button",
+  SemanticMarkButton: "semantic-button",
 };
 
 export const Colors = {
-	pastelRed: "rgba(253, 138, 138, 0.6)",
-	pastelRedActive: "rgba(253, 138, 138, 0.9)",
-	pastelYellow: "rgba(241, 247, 181, 0.6)",
-	pastelYellowActive: "rgba(241, 247, 181, 0.9)",
-	pastelGreen: "rgba(168, 209, 209, 0.6)",
-	pastelGreenActive: "rgba(168, 209, 209, 0.9)",
-	pastelBlue: "rgba(158, 161, 212, 0.6)",
-	pastelBlueActive: "rgba(158, 161, 212, 0.9)",
-	pastelGray: "rgb(128, 128, 128, 0.6)",
+  pastelRed: "rgba(253, 138, 138, 0.6)",
+  pastelRedActive: "rgba(253, 138, 138, 0.9)",
+  pastelYellow: "rgba(241, 247, 181, 0.6)",
+  pastelYellowActive: "rgba(241, 247, 181, 0.9)",
+  pastelGreen: "rgba(168, 209, 209, 0.6)",
+  pastelGreenActive: "rgba(168, 209, 209, 0.9)",
+  pastelBlue: "rgba(158, 161, 212, 0.6)",
+  pastelBlueActive: "rgba(158, 161, 212, 0.9)",
+  pastelGray: "rgb(128, 128, 128, 0.6)",
 };
 
 export const IconsSrc = {
-	Marker: "./assets/icons/format_ink_highlighter.svg",
-	Delete: "./assets/icons/delete.svg",
+  Marker: "./assets/icons/format_ink_highlighter.svg",
+  Delete: "./assets/icons/delete.svg",
 };
 
 export const OffsetY = 16;
 
 export const GlobalEvents = {
-	ButtonClicked: "ButtonClicked",
-	CollectionClicked: "CollectionClicked",
-	DeleteClicked: "DeleteClicked",
+  ButtonClicked: "ButtonClicked",
+  CollectionClicked: "CollectionClicked",
+  DeleteClicked: "DeleteClicked",
 };
 
 export const MAX_DEPTH = 100;
