d7aab341f14870e3059288579ad18929be3b4eeb
Thierry Jegen <jegenthierry@gmail.com>
2024-03-21 20:41:16 +0100

    Initial push

--- extension-logic-v2/src/services/api/apiMarkService.ts
+++ extension-logic-v2/src/services/api/apiMarkService.ts
@@ -1,8 +1,14 @@
 import { API_URL } from "../../config/constants";
 
 class apiMarkService {
-  private apiUrlBase = `${API_URL}/Mark`;
-  public async fetchExistingMarks(userId: string, url: string): Promise<void> {}
+  public async fetchExistingMarks(userId: string, windowUrl: string): Promise<void> {
+    const url = new URL(API_URL);
+    url.searchParams.append('userId', userId);
+    url.searchParams.append('link', windowUrl);
+    
+    const res = await fetch(url.toString());
+    const data = res.json();
+  }
 }
 
 export default new apiMarkService();
